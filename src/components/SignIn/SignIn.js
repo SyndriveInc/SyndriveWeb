a30d65724d0a681b3424c2def3ed6d6b40d4e997
SrividyaKK <SrividyaKK@users.noreply.github.com>
2018-11-02 18:12:46 +0530

    added more packages and sign in component edits

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,44 +1,66 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import InputLabel from '@material-ui/core/InputLabel';
-import Input from '@material-ui/core/Input';
+import PropTypes from 'prop-types';
 import './SignIn.css';
+import { withStyles, Button, Input, InputLabel } from '@material-ui/core';
+import authComponentStyle from '../../assets/jss/authComponentStyle';
 
-const SignIn = ({  }) => {
-	return (
-		<article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center card'>
-			<main className='pa4 white'>
-			  <div className='measure'>
-			    <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
-			      <legend className='f1 fw6 ph0 mh0 blue'>Sign In</legend>
-			      <div className='mt3'>
-			        <input className='pa2 input-reset ba bg-transparent hover-black w-100 field' 
-			        	type='email' name='email-address'  id='email-address'
-			        	placeholder='Email'
-			        />
-			        {/*<Input />*/}
-			      </div>
-			      <div className='mv3'>
-			        <input className='b pa2 input-reset ba bg-transparent hover-black w-100 field' 
-			        	type='password' name='password'  id='password'
-			        	placeholder='Password'
-			        />
-			    {/*<Input />*/}
-			      </div>
-			    </fieldset>
-			    <div className=''>
-			      	<Button variant='contained'
-			      		color='primary'>
-				    		Sign In
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+        }
+    }
+
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    };
+
+    render() {
+        const { classes, theme } = this.props;
+        return (
+            <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center card'>
+                <main className='pa4 white'>
+                    <div className='measure'>
+                        <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
+                            <legend className='f1 fw6 ph0 mh0 blue'>Sign In</legend>
+                            <div className='mt3'>
+                                <input className='pa2 input-reset ba bg-transparent hover-black w-100 field'
+                                    type='email' name='email-address' id='email-address'
+                                    placeholder='Email'
+                                />
+                                {/*<Input />*/}
+                            </div>
+                            <div className='mv3'>
+                                <input className='b pa2 input-reset ba bg-transparent hover-black w-100 field'
+                                    type='password' name='password' id='password'
+                                    placeholder='Password'
+                                />
+                                {/*<Input />*/}
+                            </div>
+                        </fieldset>
+                        <div className=''>
+                            <Button variant='contained'
+                                color='primary'>
+                                Sign In
 				  	</Button>
-			    </div>
-			    <div className='lh-copy mt3'>
-			      <p className='f6 link dim black pointer db blue'>Sign up</p>
-			    </div>
-			  </div>
-			</main>
-		</article>
-	);
+                        </div>
+                        <div className='lh-copy mt3'>
+                            <p className='f6 link dim black pointer db blue'>Sign up</p>
+                        </div>
+                    </div>
+                </main>
+            </article>
+        );
+    }
 }
 
-export default SignIn;
+SignIn.propTypes = {
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(authComponentStyle, { withTheme: true })(SignIn);
