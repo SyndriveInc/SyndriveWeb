ca0d917cc36d3d12763c137014719efd17a9ad18
SrividyaKK <SrividyaKK@users.noreply.github.com>
2018-11-04 07:37:53 +0530

    Some more changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import 'tachyons';
 import registerServiceWorker from './registerServiceWorker'; 
 
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+// import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import { green } from '@material-ui/core/colors';
 
 // import './firebase/firebase';
@@ -15,18 +15,18 @@ import configureStore from './store/configureStore';
 
 const store = configureStore();
 
-const theme = createMuiTheme({
-    palette: {
-        primary: green,
-    },
-});
+// const theme = createMuiTheme({
+//     palette: {
+//         primary: green,
+//     },
+// });
 
 const jsx = (
-    <Provider store={store}>
-        <MuiThemeProvider theme={theme}>
+    // <MuiThemeProvider theme={theme}>
+        <Provider store={store}>
             <App />
-        </MuiThemeProvider>
-    </Provider>
+        </Provider>
+    // </MuiThemeProvider>
 );
 
 ReactDOM.render(jsx, document.getElementById('root'));
